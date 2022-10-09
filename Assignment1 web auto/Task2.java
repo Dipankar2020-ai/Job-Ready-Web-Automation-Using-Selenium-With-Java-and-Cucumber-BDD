0dba4f85c0494b5e3d7c8fa59a450fa6fedb8ca1
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-10-09 17:53:27 +0530

    Add files via upload

---
+++ Assignment1 web auto/Task2.java
@@ -0,0 +1,27 @@
+//Write a program to print the sum of below 5 numbers.
+//	10,90.78,111,8989,7876
+
+package Assignment1;
+
+
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		double b=90.78;
+		int c=111;
+		int d=8989;
+		int e=7876;
+		
+		double sum=a+b+c+d+e;
+		System.out.println("The sum of below 5 numbers is "+sum);
+		
+		
+		
+
+	}
+
+}
