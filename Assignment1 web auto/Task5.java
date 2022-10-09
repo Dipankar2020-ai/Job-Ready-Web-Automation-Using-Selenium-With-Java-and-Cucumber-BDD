87ef809642da04a8cf41bda9102c66e3141d8ba4
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-10-09 18:17:29 +0530

    Add files via upload

---
+++ Assignment1 web auto/Task5.java
@@ -0,0 +1,20 @@
+//Task 5- Write a program to print all odd numbers from 1-50
+package Assignment1;
+
+public class Task5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing all odd numbers from 1-50:");
+		for(int i=1;i<=50;i++)
+		{
+			if(i%2==1)
+			{
+				System.out.print(i+" ");
+			}
+		}
+
+
+	}
+
+}
