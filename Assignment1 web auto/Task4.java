b8679dfaa1c375a9550d9b5f1efa509c9e1bd17b
Dipankar2020-ai <63770793+Dipankar2020-ai@users.noreply.github.com>
2022-10-09 18:13:58 +0530

    Add files via upload

---
+++ Assignment1 web auto/Task4.java
@@ -0,0 +1,19 @@
+//Task 4- Write a program to print all even numbers from 1-200
+package Assignment1;
+
+public class Task4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Printing all even numbers from 1-200:");
+		for(int i=1;i<=200;i++)
+		{
+			if(i%2==0)
+			{
+				System.out.print(i+" ");
+			}
+		}
+
+	}
+
+}
